7ab0abfc862f9a0d3ff794bbad205f3f3423ae9f
Christian Castaneda <castaneda_garcia.christian_jairo@courrier.uqam.ca>
2024-06-27 02:11:56 -0400

    Project setup

---
+++ src/main/java/crhisjairo/tools/vanillaminecraftserverhelper/HelloApplication.java
@@ -0,0 +1,30 @@
+package crhisjairo.tools.vanillaminecraftserverhelper;
+
+import io.graversen.minecraft.rcon.service.ConnectOptions;
+import io.graversen.minecraft.rcon.service.MinecraftRconService;
+import io.graversen.minecraft.rcon.service.RconDetails;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+
+        final MinecraftRconService minecraftRconService = new MinecraftRconService(
+                RconDetails.localhost("password"),
+                ConnectOptions.defaults());
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module crhisjairo.tools.vanillaminecraftserverhelper {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires minecraft.rcon;
+
+    opens crhisjairo.tools.vanillaminecraftserverhelper to javafx.fxml;
+    exports crhisjairo.tools.vanillaminecraftserverhelper;
+}
