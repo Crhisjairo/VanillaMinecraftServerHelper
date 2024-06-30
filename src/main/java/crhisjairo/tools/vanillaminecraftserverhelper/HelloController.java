53897e1038bdba3b365f51d68ac32502b4de0919
Christian Castaneda <castaneda_garcia.christian_jairo@courrier.uqam.ca>
2024-06-29 20:58:37 -0400

    Connection page added

--- src/main/java/crhisjairo/tools/vanillaminecraftserverhelper/HelloController.java
+++ src/main/java/crhisjairo/tools/vanillaminecraftserverhelper/HelloController.java
@@ -2,13 +2,18 @@ package crhisjairo.tools.vanillaminecraftserverhelper;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private TextField ipField;
+
+    @FXML
+    private PasswordField pwdField;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
     }
 }
