65132fb061b7797b5bd527a5b33ff39e18d86839
Christian Castaneda <crhisjairo@hotmail.com>
2024-07-04 02:50:48 -0400

    RCON connection setup

--- src/main/java/crhisjairo/tools/vanillaminecraftserverhelper/HelloApplication.java
+++ src/main/java/crhisjairo/tools/vanillaminecraftserverhelper/HelloApplication.java
@@ -1,5 +1,6 @@
 package crhisjairo.tools.vanillaminecraftserverhelper;
 
+import io.graversen.minecraft.rcon.MinecraftRcon;
 import io.graversen.minecraft.rcon.service.ConnectOptions;
 import io.graversen.minecraft.rcon.service.MinecraftRconService;
 import io.graversen.minecraft.rcon.service.RconDetails;
@@ -9,19 +10,16 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.time.Duration;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 400, 230);
-        stage.setTitle("Hello!");
+        stage.setTitle("Vanilla Minecraft Server Helper");
         stage.setScene(scene);
         stage.show();
-
-        final MinecraftRconService minecraftRconService = new MinecraftRconService(
-                RconDetails.localhost("password"),
-                ConnectOptions.defaults());
     }
 
     public static void main(String[] args) {
--- src/main/java/crhisjairo/tools/vanillaminecraftserverhelper/HelloController.java
+++ src/main/java/crhisjairo/tools/vanillaminecraftserverhelper/HelloController.java
@@ -1,19 +1,60 @@
 package crhisjairo.tools.vanillaminecraftserverhelper;
 
+import io.graversen.minecraft.rcon.MinecraftRcon;
+import io.graversen.minecraft.rcon.service.ConnectOptions;
+import io.graversen.minecraft.rcon.service.MinecraftRconService;
+import io.graversen.minecraft.rcon.service.RconDetails;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+
+import java.time.Duration;
 
 public class HelloController {
     @FXML
     private TextField ipField;
 
     @FXML
+    private TextField port;
+
+    @FXML
     private PasswordField pwdField;
 
     @FXML
     protected void onHelloButtonClick() {
+        if(pwdField.getText().isEmpty()){
+            System.err.println("Error: Password cannot be empty!");
 
+            return;
+        }
+
+        RconDetails details;
+
+        if(ipField.getText().isEmpty()) {
+            details = RconDetails.localhost(pwdField.getText());
+            System.out.println("IP missing. Loading defaults details!");
+        } else {
+            // TODO: add int parsing verification
+            details = new RconDetails(
+                    ipField.getText(),
+                    Integer.parseInt(port.getText()),
+                    pwdField.getText());
+        }
+
+        final MinecraftRconService minecraftRconService = new MinecraftRconService(
+                details,
+                ConnectOptions.defaults());
+
+        try {
+            minecraftRconService.connectBlocking(Duration.ofSeconds(5)); // Tiempo antes de seguir
+            final MinecraftRcon minecraftRcon = minecraftRconService.minecraftRcon()
+                    .orElseThrow(IllegalStateException::new);
+        } catch (IllegalStateException e) {
+            System.err.println("Error connecting. Check ip or password.");
+        }
+
+        System.out.println("Connected!");
     }
+
 }
